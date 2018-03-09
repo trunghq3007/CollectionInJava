11cf34013faccde16cb62396b960b7cbeacaaccf
ha quang trung <haquangtrungceo@gmail.com>
2018-03-09 16:59:36 +0700

    update change

---
+++ src/collection/main/SinhVien.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package collection.main;
+
+/**
+ * @description:
+ * @author: Admin
+ * @time: 10:02:14 PM
+ * @date: Mar 7, 2018
+ */
+public class SinhVien {
+	String hoTen;
+	int tuoi;
+
+	/**
+	 * @return the hoTen
+	 */
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	/**
+	 * @param hoTen
+	 *            the hoTen to set
+	 */
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+
+	/**
+	 * @return the tuoi
+	 */
+	public int getTuoi() {
+		return tuoi;
+	}
+
+	/**
+	 * @param tuoi
+	 *            the tuoi to set
+	 */
+	public void setTuoi(int tuoi) {
+		this.tuoi = tuoi;
+	}
+}
